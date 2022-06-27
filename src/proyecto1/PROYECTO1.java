7138ec259fdfec17e04373c171b1e34197e1e6f7
Alessandro Javier Olivares Velarde <68888380+adev-web@users.noreply.github.com>
2022-06-27 02:05:25 -0500

    Up Files

---
+++ src/proyecto1/PROYECTO1.java
@@ -0,0 +1,16 @@
+
+package proyecto1;
+
+import FORMULARIOS.Login;
+import FORMULARIOS.frmPRINCIPAL;
+
+
+public class PROYECTO1 {
+
+   
+    public static void main(String[] args) {
+        Login FRM=new Login();
+        FRM.setVisible(true);
+    }
+    
+}
